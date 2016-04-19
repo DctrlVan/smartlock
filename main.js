20fe2b38ba73d47521dc3bd73bf366b7f7884632
Your Name <you@example.com>
2016-04-18 18:29:23 -0700

    file watch

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const gpio = require('onoff').Gpio
-var pin = gpio(17, 'out');
-pin.write(1, function(err){
-	setTimeout( function(){ pin.write(0) }, 3333);
-})
+const fs = require('fs');
+const CARDFILE = '~/test3.txt';
 
+function beer(){
+	var pin = gpio(17, 'out');
+	pin.write(1, function(err){
+		setTimeout( function(){ pin.write(0) }, 3333);
+	})
+}
+
+fs.watch(CARDFILE, function(cur, prev){
+	fs.stat(CARDFILE, function(err, stats){
+		var size = stats.size;
+		var readable = fs.createReadStream( CARDFILE, {start:size - 10, encoding:'utf8'} );
+		readable.on('data', (chunk) => {
+           		 console.log(chunk)
+        	});
+    });
+});
